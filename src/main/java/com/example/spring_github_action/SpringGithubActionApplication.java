4a3d56fcfd40a9dd39d43a50d1c348f16923b63c
박싱철 <zmzss0213@naver.com>
2022-06-23 04:00:08 +0900

    test

---
+++ src/main/java/com/example/spring_github_action/SpringGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_github_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_github_action/SpringGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_github_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
